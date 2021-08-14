70180ebe14dc8681b3b79c2564b6e6496d435833
kengo hirata <athlete551@icloud.com>
2021-08-15 05:00:00 +0900

    fix fallback to true in meetupDetail

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -22,7 +22,7 @@ export const getStaticPaths = async () => {
     const loadedMeetups = await collection.find({}, { _id: 1 }).toArray();
     client.close();
     return {
-        fallback: false,
+        fallback: true,
         paths: loadedMeetups.map(meetup => ({params: {meetupId: meetup._id.toString()}}))
     };
 };
