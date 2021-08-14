445f68bdbd37d2c6be6a1629305a3828419722ef
kengo hirata <athlete551@icloud.com>
2021-08-15 04:51:26 +0900

    fix revalidate prop in meetups

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export const getStaticProps = async () => {
                 description: meetup.description
             }))
         },
-        revalidate: 3600
+        revalidate: 1
     };
 };
 
